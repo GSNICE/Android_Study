5f0ea8806ef147193f45a352101e3af976363efe
Gavin <sdjngcb@gmail.com>
2020-06-29 00:18:37 +0800

    commit

--- HelloWorld/app/src/main/java/com/gsnice/helloworld/ScrollViewActivity.java
+++ HelloWorld/app/src/main/java/com/gsnice/helloworld/ScrollViewActivity.java
@@ -2,32 +2,56 @@ package com.gsnice.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ScrollView;
+import android.widget.TextView;
 
-public class ScrollViewActivity extends AppCompatActivity {
+public class ScrollViewActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button button_1;
-    private Button button_2;
+    private Button button_scroll_up;
+    private Button getButton_scroll_down;
     private ScrollView scrollView_1;
-    private Context content_1;
+    private TextView textView_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scroll_view);
-        content_1 = ScrollViewActivity.this;
         bindViews();
     }
 
     public void bindViews () {
-        button_1 = findViewById(R.id.scroll_button_1);
-        button_2 = findViewById(R.id.scroll_button_2);
+        button_scroll_up = findViewById(R.id.scroll_button_up);
+        getButton_scroll_down = findViewById(R.id.scroll_button_down);
 
-        scrollView_1 = findViewById(R.id.ScrollView_1);
+        textView_1 = findViewById(R.id.scrollView_textView);
 
+        button_scroll_up.setOnClickListener(this);
+        getButton_scroll_down.setOnClickListener(this);
 
+        scrollView_1 = findViewById(R.id.scrollView_1);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0 ;i <= 100;i++) {
+            sb.append("呵呵 * " + i + "\n");
+        }
+        textView_1.setText(sb.toString());
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_1:
+                //
+                scrollView_1.fullScroll(ScrollView.FOCUS_DOWN);
+                break;
+            case R.id.button_2:
+                //
+                scrollView_1.fullScroll(ScrollView.FOCUS_UP);
+                break;
+        }
     }
 }
